575a2edaf53e69390f37164bf019a520daba9d48
Jobson Sena <jobsonsena.odonto@outlook.com>
2026-05-18 13:05:45 -0300

    Refactor Categoria class and improve comments

--- src/main/java/br/com/fuctura/biblioteca/models/Categoria.java
+++ src/main/java/br/com/fuctura/biblioteca/models/Categoria.java
@@ -27,38 +27,15 @@ public class Categoria implements Serializable {
     private String nome;
     private String descricao; //ok
 
-    @OneToMany(mappedBy = "categoria") //uma categoria para nenhum ou muitos livros
-    private List<Livro> livros = new ArrayList<>(); //se não inicializar, seria uma lista nula (pointexception)
+    @OneToMany(mappedBy = "categoria") //uma categoria para nenhum ou muitos livros - livro vai ser mapeado por alguma categoria.
+                                       // livro não pode exitir sem uma categoria.
+    private List<Livro> livros = new ArrayList<>(); //se não inicializar, seria uma lista nula (pointexception);
 
-    ;
 
-    public Categoria(Integer id, String nome, String descricao) {
-        this.id = id;
-        this.nome = nome;
-        this.descricao = descricao; //
-    }
+      public Categoria(Integer id, String nome, String descricao){
+          this.id = id;
+          this.nome = nome;
+          this.descricao = descricao;
 
-    public Integer getId() {
-        return id; }
-
-    public void setId(Integer id) {
-        this.id = id; }
-
-    public String getNome() {
-        return nome; }
-
-    public void setNome(String nome) {
-        this.nome = nome; }
-
-    public String getDescricao() {
-        return descricao; }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao; }
-
-    public List<Livro> getLivros() {
-        return livros;}
-
-    public void setLivros(List<Livro> livros) {
-        this.livros = livros; }
-}
+      }
+       }
