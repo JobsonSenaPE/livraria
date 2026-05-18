c5c273d4fa17db517477a763ba9eb0907cd17771
Jobson Sena <jobsonsena.odonto@outlook.com>
2026-05-18 13:07:09 -0300

    Convert CategoriaRepository to interface with JPA support

--- src/main/java/br/com/fuctura/biblioteca/repositories/CategoriaRepository.java
+++ src/main/java/br/com/fuctura/biblioteca/repositories/CategoriaRepository.java
@@ -1,4 +1,15 @@
 package br.com.fuctura.biblioteca.repositories;
 
-public class CategoriaRepository {
+import br.com.fuctura.biblioteca.models.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+    //@Query("SELECT c FROM Categoria c WHERE LOWER(c.nome) LIKE LOWER(CONCAT('%', :nome, '%'))")
+    List<Categoria> findByNomeContainingIgnoreCase(String nome);
 }
